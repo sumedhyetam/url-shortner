8a59108cc02e9590bc9ad8079335c81635c529d1
sumedhyetam <78087934+sumedhyetam@users.noreply.github.com>
2025-10-18 11:56:24 +0530

    adding file

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"SumedhY/url-shortner/api/routes"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	router := gin.Default()
+
+	setupRoutes(router)
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Fatal(router.Run(":" + port))
+}
+
+func setupRoutes(router *gin.Engine) {
+	router.POST("/shorten", routes.ShortenURL)
+	// router.GET("/:shortenedURL", routes.GetURL)
+	// router.POST("/addTag", routes.AddTag)
+	// router.POST("/editUrl", routes.EditURL)
+	// router.POST("/deleteUrl", routes.DeleteURL)
+}
